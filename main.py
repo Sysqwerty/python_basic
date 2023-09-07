37031cc420f2aa6fddb1f2a39348544b3dcae8cb
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-09-07 20:25:41 +0300

    chore: 3rd commit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-print("Hello World!")
+boss = 'Oleksii'
+if boss.__eq__('Oleksii'):
+    print(f'Hello, {boss}!')
+elif boss.__contains__('Oleksii'):
+    print(f'You are almost a boss')
+else:
+    print('Hey')
 
 a = 1
 b = 2
 c = a + b + 10
-print(c)
+# print(c)

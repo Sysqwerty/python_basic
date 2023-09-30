de095d9efeecd48b00e75a33bb385cf4b8bf23f6
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-09-30 13:41:08 +0300

    buble_sort() optimization

--- buble_sort.py
+++ buble_sort.py
@@ -1,6 +1,6 @@
 def buble_sort(array):
     for i in range(0, len(array) - 1):
-        for j in range(0, len(array) - 1):
+        for j in range(0, len(array) - i - 1):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
     return array

e8d40da51450f1b1a4aff82bacc7bf8279471871
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-09-30 13:39:41 +0300

    add buble_sort example

---
+++ buble_sort.py
@@ -0,0 +1,10 @@
+def buble_sort(array):
+    for i in range(0, len(array) - 1):
+        for j in range(0, len(array) - 1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+l = [1, 54, 0, -3, 14, -12]
+
+print(buble_sort(l))

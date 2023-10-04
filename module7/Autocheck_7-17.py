a73dd5757aade1b6f34f5248b21f4fe486e66e91
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-10-05 00:23:53 +0300

    chore: upd 7-17

--- module7/Autocheck_7-17.py
+++ module7/Autocheck_7-17.py
@@ -1,22 +1,19 @@
 def encode(data):
-    def _encode_recursive(data):
-        if not data:
-            return []
+    if not data:
+        return []
 
-        current_element = data[0]
-        count = 1
+    current_element = data[0]
+    count = 1
 
-        for i in range(1, len(data)):
-            if data[i] == current_element:
-                count += 1
-            else:
-                break
+    for i in range(1, len(data)):
+        if data[i] == current_element:
+            count += 1
+        else:
+            break
 
-        encoded_data = [current_element, count]
-        remaining_data = data[count:]
-        return encoded_data + _encode_recursive(remaining_data)
-
-    return _encode_recursive(data)
+    encoded_data = [current_element, count]
+    remaining_data = data[count:]
+    return encoded_data + encode(remaining_data)
 
 data =  ['X', 'X', 'X', 'Z', 'Z', 'X', 'X', 'Y', 'Y', 'Y', 'Z', 'Z'] # ['X', 3, 'Z', 2, 'X', 2, 'Y', 3, 'Z', 2]
 

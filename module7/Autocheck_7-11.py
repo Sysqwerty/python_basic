a7547a80420d2d6bbf6bd8761576271c3ded4af3
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-10-14 12:07:55 +0300

    update: Autocheck_7-11.py

--- module7/Autocheck_7-11.py
+++ module7/Autocheck_7-11.py
@@ -46,3 +46,31 @@ def sequence_buttons(string: str):
 
 
 print(sequence_buttons(string))
+
+
+# BUTTONS = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+# GROUPS_OF_SYMBOLS = (
+#     ".,?!:",
+#     "ABC",
+#     "DEF",
+#     "GHI",
+#     "JKL",
+#     "MNO",
+#     "PQRS",
+#     "TUV",
+#     "WXYZ",
+#     " ",
+# )
+
+# TRANSLIT_DICT = {}
+
+
+# for button, g_of_symbols in zip(BUTTONS, GROUPS_OF_SYMBOLS):
+#     for p, s in enumerate(g_of_symbols):
+#         count = p + 1
+#         TRANSLIT_DICT[ord(s.lower())] = str(button) * count
+#         TRANSLIT_DICT[ord(s)] = str(button) * count
+
+
+# def sequence_buttons(string):
+#     return string.translate(TRANSLIT_DICT)

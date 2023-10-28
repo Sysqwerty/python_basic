7291aee8ecc89829861b1108bd19bba961b7eae4
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-10-28 13:25:36 +0300

    add Game

---
+++ Game/constants.py
@@ -0,0 +1,9 @@
+SIGNS = ["0", "|", "/", "\\", "/", "\\"]
+
+BODY_PARTS = {index:value for index, value in enumerate(SIGNS)}
+
+FILENAME_MAPPING = {
+    5: 'words/fiveletterWords.txt',
+    6: 'words/sixletterWords.txt',
+    7: 'words/sevenletterWords.txt',
+}
---
+++ Game/main.py
@@ -0,0 +1,29 @@
+from models.man import Man
+
+
+import random
+
+from models.word import Word
+
+
+def main():
+    letters = random.randint(5, 7)
+    word = Word(letters)
+    man = Man()
+
+    while not man.is_game_over() and not word.is_word_guessed():
+        guess = input()
+        if word.check_letters(guess):
+            pass
+        else:
+            man.add_body_part()
+        
+    if word.is_word_guessed():
+        print("You won")
+    else:
+        print("You lose")
+
+
+
+if __name__ == '__main__':
+    main()
